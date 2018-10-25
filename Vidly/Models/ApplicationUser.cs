eda454c8794aab99ffda100d9a4d51c7a84b71cd
Juliano Hubel <juliano_hubel@hotmail.com>
2018-10-25 11:20:58 -0300

    execicio section 8

--- Vidly/Models/ApplicationUser.cs
+++ Vidly/Models/ApplicationUser.cs
@@ -1,8 +1,11 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+using System.Web;
 
 namespace Vidly.Models
 {
@@ -15,25 +18,6 @@ namespace Vidly.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
-        }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Movie> Movies{ get; set; }
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
         }
     }
 }
